1f872ffab3ee065d2f82bfaf286eae234341b5f3
WuTUT <762641171@qq.com>
2021-04-01 00:51:42 +0800

    check only disjoint

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,157 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int n;
+    private WeightedQuickUnionUF djSet;
+    private int source;
+    private int to;
+    private int[][] opened;
+    private int openNum;
 
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        n = N;
+        openNum = 0;
+        opened = new int[N][N];
+        djSet = new WeightedQuickUnionUF(N * N + 2);
+        source = n * n;
+        to = n * n + 1;
+    }
+
+    private boolean isOutOfBound(int nn) {
+        if (nn < 0 || nn >= n) {
+            return true;
+        }
+        return false;
+    }
+
+    private int xyTo1d(int row, int col) {
+        return row * n + col;
+    }
+
+    /**
+     * open the site (row, col) if it is not open already
+     *
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        if (isOutOfBound(row) || isOutOfBound(col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        int indexTarget = xyTo1d(row, col);
+        if (opened[row][col] == 0) {
+            opened[row][col] = 1;
+            openNum++;
+            if (row == 0) {
+                djSet.union(indexTarget, source);
+            }
+            if (row == n - 1) {
+                djSet.union(indexTarget, to);
+            }
+            if (row > 0 && opened[row - 1][col] != 0) {
+                djSet.union(xyTo1d(row - 1, col), indexTarget);
+            }
+            if (row < n - 1 && opened[row + 1][col] != 0) {
+                djSet.union(xyTo1d(row + 1, col), indexTarget);
+            }
+            if (col > 0 && opened[row][col - 1] != 0) {
+                djSet.union(xyTo1d(row, col - 1), indexTarget);
+            }
+            if (col < n - 1 && opened[row][col + 1] != 0) {
+                djSet.union(xyTo1d(row, col + 1), indexTarget);
+            }
+        }
+
+
+    }
+
+    /**
+     * is the site (row, col) open?
+     *
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        if (isOutOfBound(row) || isOutOfBound(col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return opened[row][col] != 0;
+    }
+
+    /**
+     * is the site (row, col) full?
+     *
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        if (isOutOfBound(row) || isOutOfBound(col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (row != n - 1) {
+            return djSet.connected(xyTo1d(row, col), source);
+        } else {
+            if (!djSet.connected(xyTo1d(row, col), source)) {
+                return false;
+            }
+            return djSet.connected(xyTo1d(row, col), source);
+        }
+    }
+
+    /**
+     * number of open sites
+     *
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return openNum;
+    }
+
+    /**
+     * does the system percolate?
+     *
+     * @return
+     */
+    public boolean percolates() {
+        return djSet.connected(source, to);
+    }
+
+    /**
+     * use for unit testing
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Percolation percolation = new Percolation(5);
+        System.out.println(percolation.percolates());
+        percolation.open(3, 4);
+        System.out.println(percolation.numberOfOpenSites());
+        percolation.open(2, 4);
+        percolation.open(2, 2);
+        System.out.println(percolation.numberOfOpenSites());
+        percolation.open(2, 3);
+        System.out.println(percolation.numberOfOpenSites());
+        percolation.open(0, 2);
+        System.out.println(percolation.numberOfOpenSites());
+        percolation.open(1, 2);
+        System.out.println(percolation.isFull(2, 2));
+        System.out.println(percolation.isOpen(0, 2));
+        System.out.println(percolation.percolates());
+        percolation.open(2, 4);
+        System.out.println(percolation.percolates());
+        percolation.open(4, 4);
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+
+        Percolation percolation1 = new Percolation(1);
+        System.out.println(percolation1.percolates());
+        percolation1.open(0, 0);
+        System.out.println(percolation1.numberOfOpenSites());
+        System.out.println(percolation1.percolates());
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,70 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
 
+public class PercolationStats {
+    /**
+     * perform T independent experiments on an N-by-N grid
+     *
+     * @param N
+     * @param T
+     * @param pf
+     */
+    private double sumX = 0;
+    private double sumX2 = 0;
+    private int examT;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        examT = T;
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = pf.make(N);
+            int count = 0;
+            while (!percolation.percolates()) {
+                int row, col;
+                do {
+                    row = StdRandom.uniform(N);
+                    col = StdRandom.uniform(N);
+                } while (!percolation.isOpen(row, col));
+                percolation.open(row, col);
+                count++;
+            }
+            sumX += count;
+            sumX2 += count * count;
+        }
+    }
+
+    public double mean() {
+        return sumX / examT;
+    }
+
+    /**
+     * sample mean of percolation threshold
+     *
+     * @return
+     */
+    public double stddev() {
+        double total = sumX2 - 2 * mean() * sumX + examT * mean() * mean();
+        return Math.sqrt(total / (examT - 1));
+    }
+
+    /**
+     * sample standard deviation of percolation threshold
+     *
+     * @return
+     */
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(examT);
+    }
+
+    /**
+     * low endpoint of 95% confidence interval
+     *
+     * @return
+     */
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(examT);
+    }
 }
