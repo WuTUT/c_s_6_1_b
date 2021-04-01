1a8b42e9971ccf197820828619283778f62957d6
WuTUT <762641171@qq.com>
2021-04-01 17:24:52 +0800

    hw3

--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -1,6 +1,7 @@
 package hw3.hash;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -33,20 +34,44 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
+    /*  Create a list of Complex Oomages called deadlyList
      * that shows the flaw in the hashCode function.
      */
-    /*
+
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
 
         // Your code here.
+        for (int i = 0; i <= 15; i++) {
+            ArrayList<Integer> param1 = new ArrayList<>();
+            ArrayList<Integer> param2 = new ArrayList<>();
+            for (int j = 0; j <= 10; j++) {
+                for (int k = 0; k <= 10; k++) {
+                    for (int l = 0; l <= 10; l++) {
+                        param1.add(i);
+                        param1.add(j);
+                        param1.add(k);
+                        param1.add(l);
+                        param1.add(0);
 
+                        param2.add(0);
+                        param2.add(j);
+                        param2.add(k);
+                        param2.add(l);
+                        param2.add(i);
+                    }
+                }
+            }
+            deadlyList.add(new ComplexOomage(param1));
+            deadlyList.add(new ComplexOomage(param2));
+        }
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
-    /** Calls tests for SimpleOomage. */
+    /**
+     * Calls tests for SimpleOomage.
+     */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestComplexOomage.class);
     }
