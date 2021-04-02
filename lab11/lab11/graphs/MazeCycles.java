ffae0f65c61c7036861f6d20a0d935f8644e77b8
WuTUT <762641171@qq.com>
2021-04-02 12:30:08 +0800

    lab11

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,7 @@
 package lab11.graphs;
 
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -10,15 +10,48 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private boolean founded;
+
     public MazeCycles(Maze m) {
         super(m);
+        founded = false;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        for (int i = 0; i < maze.V(); i++) {
+            if (!marked[i]) {
+                distTo[i] = 0;
+                edgeTo[i] = i;
+                dfs(i, -1);
+                if (founded) {
+                    break;
+                }
+            }
+        }
+       //System.out.println(founded);
     }
 
+    private void dfs(int v, int fa) {
+        if (founded) {
+            //announce();
+            return;
+        }
+        if(marked[v]){
+            return;
+        }
+        marked[v] = true;
+
+        for (int w : maze.adj(v)) {
+            if (marked[w] && w != fa) {
+                founded = true;
+                edgeTo[w] = v;
+                distTo[w] = distTo[v] + 1;
+                return;
+            }
+            dfs(w, v);
+        }
+    }
     // Helper methods go here
 }
 
